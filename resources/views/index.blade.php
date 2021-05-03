a4a9e7b93748a83c891c074822bb581cb7efa5c1
Nish <dulan418@gmail.com>
2021-05-03 21:48:10 +0530

    ffchange

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -100,23 +100,7 @@
     }
 
 
-     #loader {
-         border: 16px solid #f3f3f3; /* Light grey */
-         border-top: 16px solid #3498db; /* Blue */
-         border-radius: 50%;
-         width: 120px;
-         height: 120px;
-         animation: spin 2s linear infinite;
-     }
 
-    @keyframes spin {
-        0% { transform: rotate(0deg); }
-        100% { transform: rotate(360deg); }
-    }
-
-    .hide-loader{
-        display:none;
-    }
 
 
 </style>
@@ -492,7 +476,7 @@
 
     function load(id)
         {
-            document.getElementById('loader').style.display='display'
+
         var mainwork = document.getElementById('mainwork')
         mainwork.src = "img/portfolio/works/"+id+".jpg";
         mainwork.style.visibility='visible'
